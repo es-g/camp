b02caebc9db0b153e176f35a9d6353ce895eab88
Yesbol <esbolpro@gmail.com>
2019-08-04 18:10:08 +0000

    amended port

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
 	console.log("Yelp server has started");
 });
